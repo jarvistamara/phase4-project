c8a7a9eefdac72fa283d889b0f12346f9b41b1a4
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-06 23:02:08 -0400

    added signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react'
 
 const Signup = () => {
-    
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
-
-
-
-
+    const handleSubmit = (e) => {
+        // prevents a sending of a post request on submit
+        e.preventDefault()
+        const configOb = {
+            method: 'POST', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                passwordConfirmation: passwordConfirmation
+            })
+        }
+        fetch('/signup', configOb)
+    }
 
     return (
         <div>
-            <h3>SIGNUP</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Username:</label>
+                <input type='text' id='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <br/><br/>
+                <label>Password:</label>
+                <input type='text' id='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <br/><br/>
+                <label>Password Confirmation:</label>
+                <input type='text' id='password_confirmation' value={password_confirmation} onChange={(e) => setPasswordConfirmation(e.target.value)}/>
+                <input type='submit' />
+            </form>
         </div>
     )
 }
