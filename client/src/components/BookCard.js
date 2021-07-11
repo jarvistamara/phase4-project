ff3ed374d5db14fe330ad86f3a978efe36c7ba2f
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-11 00:46:17 -0400

    working on book component

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Book from '../containers/Book'
 
 const BookCard= ({books}) => {
+    // const [book, setBook] = useState([])
+    // const [errors, setErrors] = useState('')
 
+    
     return (
         <div>
             <div className="row">
--- client/src/containers/Book.js
+++ client/src/containers/Book.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Books from './Books'
 
 const Book = () => {
+    const [book, setBook] = useState([])
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        fetch(`/books/${props.match.params.id}`)
+        .then((res) => {
+            if (res.ok) {
+                res.json()
+                .then((books) => {
+                    setBooks(books)
+                })
+            } else {
+                setError(error)
+                console.log(error)
+            }
+        })
+    }, [props.match.params.id])
+
+    const deleteBook = (id) => {
+        const headerConfig = { method: 'DELETE', headers: { 'Content-Type': 'application/json'}}
+        fetch(`/books/${id}`, headerConfig)
+        .then((res) => {
+            if (res.ok) {
+                res.json()
+                .then((newBook) => {
+                    const updatedBook = book.map(b => b.id === id ? newBook : b)
+                    setBooks({
+                        ...book,
+                        updatedBook
+                    })
+                })
+            } else {
+                setError(error)
+                console.log(error)
+            }
+        })
+    }
+
+    const editBook = (book) => {
+        const headerConfig = { method:}
+        
+    }
+
     return (
         <div>
-            <h1>test</h1>
+            <h1>TESTING</h1>
         </div>
     )
 }
